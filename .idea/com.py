a8b75eab5aa06db6422ba7c9e8b6d64e067c28c5
Benni <>
2021-06-03 11:16:12 +0200

    com Klasse geschrieben

--- .idea/com.py
+++ .idea/com.py
@@ -4,3 +4,16 @@ class COM(Serial):
 
     def __init__(self):
         super().__init__()
+        self.terminierung = ""
+
+    def get_terminierung(self):
+        return self.terminierung
+
+    def set_port(self):
+        pass
+
+    def set_terminierung(self, zeichen):
+        self.terminierung = zeichen
+
+    def write(self, msg):
+        super().write((msg + self.terminierung).encode("utf-8"))
--- .idea/main.py
+++ .idea/main.py
@@ -1,5 +1,22 @@
 # Press Umschalt+F10 to execute it or replace it with your code.
-
+from com import COM
+from time import sleep
 
 if __name__ == '__main__':
-    pass
+    com=COM()
+    com.port = "COM5"
+    com.baudrate = 9600
+    com.set_terminierung("\r")
+    com.timeout = 0.3
+    com.open()
+
+    sleep(3)
+
+    com.write("an")
+    print(com.readline())
+    sleep(2)
+    com.write("aus")
+    print(com.readline())
+    sleep(2)
+
+    com.close()
