646cd7124b15904fade8a3ec013da9520f5466a5
Benni <>
2021-07-13 14:28:14 +0200

    median klasse hinzugefügt

---
+++ median.py
@@ -0,0 +1,22 @@
+from statistics import median
+
+class Median():
+
+    def __init__(self, breite=10):
+        self.ungefiltert = []
+        self.gefiltert = []
+        self.fenster_breite = breite
+
+    def filtern(self, werte):
+        self.gefiltert = []
+        self.ungefiltert = werte
+        j = 0
+        tmp = 0
+        for i, elem in enumerate(self.ungefiltert):
+            tmp = 0
+            if((i+self.fenster_breite) < (len(self.ungefiltert)+1)):
+                tmp = median(self.ungefiltert[i:i+self.fenster_breite])
+                self.gefiltert.append(tmp)
+            else:
+                break
+        return self.gefiltert
